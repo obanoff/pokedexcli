d664b6d4a67fda287b642b77393fbffb91d3ef67
Yevhen Shliakhovskyi <obanoff@gmail.com>
2024-08-17 23:44:40 +0300

    Add internals/models/command.go
        -- added cliCommand struct {name, description string, callback func() error} representing a command
        -- added CommandRegistry map[string]cliCommand to store commands
        -- added methods on CommandRegistry:
            func NewCommandRegistry() CommandRegistry creates a new command
            registry
            func (cd CommandRegistry) AddCommand(name, description string,
            callback func() error)
            func (cd CommandRegistry) Run(name) error to check the command
            existence and run if possible or throw an the error
    
    Add reading commands from os.Stdin and executing appropriate callbacks, if any, logic to main.go

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/obanoff/pokedexcli/internals/models"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	cmdRegistry := models.NewCommandRegistry()
+
+	cmdRegistry.AddCommand("help", "prints a help message describing how to use the REPL", func() error {
+		fmt.Println("Usege: ")
+		return nil
+	})
+
+	cmdRegistry.AddCommand("exit", "exits the program", func() error {
+		os.Exit(0)
+		return nil
+	})
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("pokedex > ")
+
+		scanner.Scan()
+		err := scanner.Err()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		cmd := scanner.Text()
+
+		err = cmdRegistry.Run(cmd)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+	}
 }
